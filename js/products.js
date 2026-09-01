f85aeb30ac09e50e89248ce2d4222d65a7987005
movielover66 <tukaisana88@gmail.com>
2026-09-01 23:12:24 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,7 +6,7 @@ const FALLBACK_PRODUCTS = [
     id: 1,
     name: "KTM WHITE 390",
     category: "T-Shirts",
-    price: 1,
+    price: 549,
     compareAt: 899,
     badge: "Best Seller",
     orderType: "collection",
